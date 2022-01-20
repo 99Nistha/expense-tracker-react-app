04383d483739c51020b6920eaf0c9c6cddef7878
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-20 12:37:49 +0530

    renaming

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return ( 
     <div>
       <h2>Let's get started!</h2>
-      <Expenses expenseData={expenses}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,24 +5,24 @@ function Expenses(props){
     return(
         <div className="expenses">
         <ExpenseItem 
-            title={props.expenseData[0].title} 
-            amount={props.expenseData[0].amount} 
-            date={props.expenseData[0].date}
+            title={props.items[0].title} 
+            amount={props.items[0].amount} 
+            date={props.items[0].date}
         />
         <ExpenseItem 
-            title={props.expenseData[1].title} 
-            amount={props.expenseData[1].amount} 
-            date={props.expenseData[1].date}
+            title={props.items[1].title} 
+            amount={props.items[1].amount} 
+            date={props.items[1].date}
         />
         <ExpenseItem 
-            title={props.expenseData[2].title} 
-            amount={props.expenseData[2].amount} 
-            date={props.expenseData[2].date}
+            title={props.items[2].title} 
+            amount={props.items[2].amount} 
+            date={props.items[2].date}
         />
         <ExpenseItem 
-            title={props.expenseData[3].title} 
-            amount={props.expenseData[3].amount} 
-            date={props.expenseData[3].date}
+            title={props.items[3].title} 
+            amount={props.items[3].amount} 
+            date={props.items[3].date}
         />
       </div>
     );
