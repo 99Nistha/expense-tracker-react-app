cd1039fa8f15d78d9c751733aa39f0eef614cfd8
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-22 14:32:23 +0530

    Organizing Component Files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 import React from "react";
 
 function App() {
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import Card from "./Card";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
