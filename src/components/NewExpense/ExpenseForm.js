46c5d6c6780287337c82449e72574353765e51a8
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-22 18:27:54 +0530

    Updating State using the very first one

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,43 +3,43 @@ import './ExpenseForm.css';
 
 const ExpenseForm =() =>{
 
-    // const [enteredTitle, setEnternedTitle]= useState(''); // we can have multiple state, and are independent of each other
-    // const [enteredAmount, setEnternedAmount]= useState('');
-    // const [enteredDate, setEnternedDate]= useState('');
+    const [enteredTitle, setEnternedTitle]= useState(''); // we can have multiple state, and are independent of each other
+    const [enteredAmount, setEnternedAmount]= useState('');
+    const [enteredDate, setEnternedDate]= useState('');
 
-    const [userInput, setUserInput]= useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    })
+    // const [userInput, setUserInput]= useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
 
     const titleChangeHandle = (event) =>{
-        // setEnternedTitle(event.target.value);  // we are using the setSate so that the value could be stored even if the entire conponents re evaluate for some reason
+        setEnternedTitle(event.target.value);  // we are using the setSate so that the value could be stored even if the entire conponents re evaluate for some reason
         // setUserInput({
         //     ...userInput,  //we are using ...userInput, to set the previous value and then in the next line we are just overwritting the value of title
         //     enteredTitle: event.target.value
         // });
 
-        setUserInput((prevState)=>{
-            return{...prevState, enteredTitle: event.target.value};
-        });
+        // setUserInput((prevState)=>{
+        //     return{...prevState, enteredTitle: event.target.value};
+        // });
         //this one is better bcz we are making sure that we are picking the latest previous values and not something impacted by scheduled time
     };
 
     const amountChangeHandle = (event) =>{
-        // setEnternedAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
-        });
+        setEnternedAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // });
     };
 
     const dateChangeHandle = (event) =>{
-        // setEnternedDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
-        });
+        setEnternedDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // });
     };
 
     return <form>
