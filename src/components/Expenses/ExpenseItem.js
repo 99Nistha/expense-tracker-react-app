cc7c45ca93f977a4345102c557ad4733c143ba67
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-22 16:58:05 +0530

    Listening to Events & Working with Event Handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,11 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+
+    const clickHandler = () =>{
+        console.log("Clicked!!!!");
+    };
+    
     return (
         <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
@@ -10,6 +15,7 @@ const ExpenseItem = (props) => {
             <h2>{props.title}</h2>
             <div className='expense-item__price'>₹{props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
         </Card>
     ); 
 }
