af4ca71ad2a6137f2e12d888768c23990f1b13e2
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-22 17:18:17 +0530

    Working with "State"

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import React, {useState} from 'react';
 
 const ExpenseItem = (props) => {
 
+    const [title, setTitle]= useState(props.title); // should we called just directly into our componennt, and should start with use => useState is a HOOK
+
     const clickHandler = () =>{
-        console.log("Clicked!!!!");
+        setTitle('Update');  //revaluate the entire component again, ontly this one
+        console.log(title); // will be uodate after a schedule time, will take few millisec not imid.
     };
-    
+
     return (
         <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>₹{props.amount}</div>
         </div>
         <button onClick={clickHandler}>Change Title</button>
