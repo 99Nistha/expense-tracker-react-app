84b24dd8406e58c3333e637d7c4f6418772bd86e
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-26 21:56:24 +0530

    Using Stateful Lists- update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Expenses from "./components/Expenses/Expenses";
 import React from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react/cjs/react.development";
 
-function App() {
-  const expenses=[{title: 'Laptop', amount: 105000.00, date: new Date(2021, 6, 25)},
+const dummy_Expenses=[{title: 'Laptop', amount: 105000.00, date: new Date(2021, 6, 25)},
                   {title: 'Internet Bill', amount: 12300.99, date: new Date(2022, 0, 1)},
                   {title: 'Wooden Table', amount: 1200.34, date: new Date(2022, 0, 16)},
                   {title: 'New Mobile', amount: 50000.00, date: new Date(2021, 9, 10)}]; 
 
+function App() {
+  const [expenses, setExpenses] =useState(dummy_Expenses);
   const addExpenseHandler =(expenseData) =>{
-    console.log(expenseData);
+    setExpenses((prevExpense)=>{
+      return [expenseData, ...prevExpense];
+    });
   }
 
   return ( 
