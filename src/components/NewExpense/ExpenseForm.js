4b6a048298581a399ec673523cbec8280ab3ca56
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-29 19:16:05 +0530

    small bug fix

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -46,7 +46,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
