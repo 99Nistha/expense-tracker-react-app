d04cc6e9bc04173247c4874fac3691afd48e91ee
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-29 17:48:09 +0530

    Conditional Content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -89,6 +89,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const NewExpenseDataHandler = (expenseData) => {
     expenseData = {
       ...expenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const StartEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const StopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={NewExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={StartEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={NewExpenseDataHandler}
+          onCancel={StopEditingHandler}
+        />
+      )}
     </div>
   );
 };
