829f8b4d19498fd94391d935a459740bbcb2d33d
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-29 19:06:51 +0530

    chart bar styling and formating

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,9 +3,7 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-  const dataPointValues = props.dataPoints.map(
-    (dataPoint) => dataPoint.value
-  );
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...dataPointValues);
   return (
     <div className="chart">
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./ChartBar.css";
 
 const ChartBars = (props) => {
-  let barFillHeight = '0%';
+  let barFillHeight = "0%";
 
   if (props.maxValue > 0) {
-    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
   return (
