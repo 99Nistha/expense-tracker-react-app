9b1997e779c17f7ac5d67caec090c891d34041c6
Nistha Agarwal <nisthaagarwal8@gmail.com>
2022-01-29 16:38:05 +0530

    Outputting Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,20 +15,25 @@ const Expenses = (props) => {
     return arr.date.getFullYear().toString() === changedYear;
   });
 
+  let expenseContent = <p>No Expenses Found.</p>;
+  if (filteredExpense.length > 0) {
+    expenseContent = filteredExpense.map((expense) => (
+      <ExpenseItem
+        key={expense.id} // to let react uniquely identify data and add accordingly
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={changedYear}
         onFilterChange={filterChangeHandler}
       />
-      {filteredExpense.map((expense) => (
-        <ExpenseItem
-          key={expense.id} // to let react uniquely identify data and add accordingly
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expenseContent}
     </Card>
   );
 };
